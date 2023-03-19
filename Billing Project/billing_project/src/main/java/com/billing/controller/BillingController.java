585300965fb79a1c7eefa03ba3d3614fbee76437
ravisingh270195 <sravi0642@gmail.com>
2023-03-19 11:02:28 +0530

    dashboard

--- Billing Project/billing_project/src/main/java/com/billing/controller/BillingController.java
+++ Billing Project/billing_project/src/main/java/com/billing/controller/BillingController.java
@@ -27,4 +27,8 @@ public class BillingController {
             return "login";
         }
     }
+    @PostMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
 }
